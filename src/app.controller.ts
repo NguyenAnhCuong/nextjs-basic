855b708b0410877bbca4d20fc209beb9a31162a0
cuong <cuong1606x@gmail.com>
2025-08-05 17:07:09 +0700

    passport jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,6 +9,9 @@ import {
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './decorator/customize';
 
 @Controller()
 export class AppController {
@@ -20,11 +23,19 @@ export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
+    private authService: AuthService,
   ) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  // @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
