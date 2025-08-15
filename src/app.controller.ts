1945c93d248bc109d6a9a069e1b085798b52aa9d
cuong <cuong1606x@gmail.com>
2025-08-15 16:41:26 +0700

    upload file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
+import { Public, ResponseMessage } from "./decorator/customize";
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Public()
+  @Get("/day")
+  @ResponseMessage("get day")
+  getDay() {
+    try {
+      const day = new Date();
+      return { day };
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
+}
